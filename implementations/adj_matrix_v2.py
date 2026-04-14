29352f6aea225521880edf48492003b17cd7b11f
Erick Kanja <e.kanja@alustudent.com>
2026-04-14 11:44:27 +0300

    Add function to add edge in adj matrix

--- implementations/adj_matrix_v2.py
+++ implementations/adj_matrix_v2.py
@@ -11,7 +11,19 @@ def add_node(v):
     for _ in range(node_count):
         new_row.append(0)
     graph.append(new_row)
-
+    
+def add_edge(v1, v2):
+    if v1 not in nodes:
+        print("Not possible")
+        return
+    if v2 not in nodes:
+        print("Not possible")
+        return
+    index_v1 = nodes.index(v1)
+    index_v2 = nodes.index(v2)
+    
+    graph[index_v1][index_v2] = 1
+    graph[index_v2][index_v1] = 1
 
 
 
@@ -26,6 +38,7 @@ nodes = []
 add_node("a")
 add_node("b")
 add_node("c")
+add_edge("b", "c")
 print(graph)
 print(nodes)
 print(node_count)
