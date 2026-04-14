b0527396e122a524c0752205574a090cb705f586
Erick Kanja <e.kanja@alustudent.com>
2026-04-14 11:27:26 +0300

    Add function to add a new node to adj matrix

--- implementations/adj_matrix_v2.py
+++ implementations/adj_matrix_v2.py
@@ -1,9 +1,16 @@
 def add_node(v):
+    global node_count
     if v in graph:
         print(f"{v} is already present in graph")
         return
-    
-
+    nodes.append(v)
+    node_count += 1
+    for row in graph:
+        row.append(0)
+    new_row = []
+    for _ in range(node_count):
+        new_row.append(0)
+    graph.append(new_row)
 
 
 
@@ -15,3 +22,10 @@ def add_node(v):
 graph = []
 node_count = 0
 nodes = []
+
+add_node("a")
+add_node("b")
+add_node("c")
+print(graph)
+print(nodes)
+print(node_count)
